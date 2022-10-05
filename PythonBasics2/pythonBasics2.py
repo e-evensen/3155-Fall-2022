c4de9b4b12d141899a9719cb0bc9701f6c1c8dff
Elijah Evensen <eevensen@uncc.edu>
2022-10-04 20:22:04 -0400

    PythonBasics2 - Update methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -15,17 +15,17 @@ from multiprocessing.dummy.connection import families
 
 def count_threes(n):
   # YOUR CODE HERE
-  threedict = {
+  threedict = { #Creates dictionary
     3: 0,
     6: 0,
     9: 0
   }
-  for i in range(0, len(n)):
-    num = int(n[i])
-    if (num == 3 or num == 6 or num == 9):
-      threedict[num] += 1
+  for i in range(0, len(n)): #Loops through the received number
+    num = int(n[i]) #Gets the current number from the received number and puts it into an int (casts it because the received number is a string)
+    if (num == 3 or num == 6 or num == 9): #Checks if number is single digit multiple of 3
+      threedict[num] += 1 #Increases the value held by the multiple key by 1
   
-  if (threedict[3] > threedict[6] and threedict[3] > threedict[9]):
+  if (threedict[3] > threedict[6] and threedict[3] > threedict[9]): #Checks which multiple occurs most often and returns it
     return 3
   elif (threedict[6] > threedict[9]):
     return 6
@@ -38,32 +38,32 @@ def count_threes(n):
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
   # YOUR CODE HERE
-  chardict = {
+  chardict = { #Dictionary
 
   }
-  longest = 1
-  former = ''
-  currLength = 0
-  for i in range(0, len(s)):
-    current = s[i]
-    if (i > 0):
-      former = s[i-1]
+  longest = 1 #Placeholder for how long the the lognest consecutive character is
+  former = '' #Placeholder for former char
+  currLength = 0 #Placeholder for current length of consecutive char
+  for i in range(0, len(s)): #Loops through received string
+    current = s[i] #Creates a string from part of received string
+    if (i > 0): #Error prevention
+      former = s[i-1] #Sets former to previous value of string
 
-    if (former is current):
-      currLength += 1
-      if (currLength > longest):
+    if (former is current): #Checks if current char is in dictionary
+      currLength += 1 #Increases currLength by 1
+      if (currLength > longest): #If currlength is longer than longest, sets longest to currLength
         longest = currLength
     else:
-      currLength = 1
+      currLength = 1 #Resets currLength to 1
 
-    if (current in chardict):
+    if (current in chardict): #Code to add char to/update character in dictionary
       if (currLength > chardict[current]):
         chardict.update({current: currLength})
     else:
       chardict.update({current: currLength})
 
-  maxChars = [key for key, value in chardict.items() if value == longest]
-  return maxChars
+  maxChars = [key for key, value in chardict.items() if value == longest] #Gets the keys with the largest values and puts them in a list
+  return maxChars #Returns the list
 
 
 # Part C. is_palindrome
