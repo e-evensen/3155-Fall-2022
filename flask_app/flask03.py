e970671bc3be7a56a667fd4980e79416dfaf4ae5
Elijah Evensen <eevensen@uncc.edu>
2022-10-27 18:33:24 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -1,9 +1,11 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 
 # imports
+from crypt import methods
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)     # create an app
 
@@ -23,7 +25,9 @@ def get_notes():
     3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
     }
 
-    return render_template('notes.html', notes=notes)
+    a_user = {'name': 'Elijah', 'email': 'mogli@uncc.edu'}
+
+    return render_template('notes.html', notes=notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
@@ -32,12 +36,19 @@ def get_note(note_id):
     3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
     }
 
-    return render_template('note.html', note=notes[int(note_id)])
+    a_user = {'name': 'Elijah', 'email': 'mogli@uncc.edu'}
 
-@app.route('/notes/new')
+    return render_template('note.html', note=notes[int(note_id)], user=a_user)
+
+@app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
     a_user = {'name': 'Elijah', 'email': 'eevensen@uncc.edu'}
-    return render_template('new.html', user=a_user)
+
+    print('request method is ', request.method)
+    if (request.method == 'POST'):
+        return '<h1>POST method used for this request</h1>'
+    else:
+        return render_template('new.html', user=a_user)  
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
